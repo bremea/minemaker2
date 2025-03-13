97505a5e1906662af9a76fd09f1e52ac7e073ec1
bremea <bremeagames@gmail.com>
2025-03-13 18:46:19 -0400

    UI preview/sandbox page

--- libraries/ui/src/routes/+page.svelte
+++ libraries/ui/src/routes/+page.svelte
@@ -1,3 +1,7 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import ArrowButton from "$lib/components/input/buttons/arrowButton.svelte";
+
+</script>
+<h1>Minemaker UI Library</h1>
+
+<ArrowButton>Hello World</ArrowButton>
