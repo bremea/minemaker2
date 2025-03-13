3adc646e254c2a408cc7e1e5f799ba1648a4d8ee
bremea <bremeagames@gmail.com>
2025-03-13 19:41:27 -0400

    include global css in ui exports

--- libraries/ui/src/lib/index.ts
+++ libraries/ui/src/lib/index.ts
@@ -1,2 +1,3 @@
 export * from "./components/input/index.js";
 export * from "./components/navigation/index.js";
+export * from './global.css';
--- libraries/ui/src/routes/+layout.svelte
+++ libraries/ui/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import '../app.css';
+	import '$lib/global.css';
 	let { children } = $props();
 </script>
 
