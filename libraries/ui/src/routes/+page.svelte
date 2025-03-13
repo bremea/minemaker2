f565116251f42f550b949784d7a4986aed4f51b3
bremea <bremeagames@gmail.com>
2025-03-13 18:47:43 -0400

    rename arrow button to arrow button left

--- libraries/ui/src/routes/+page.svelte
+++ libraries/ui/src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script>
-	import ArrowButton from "$lib/components/input/buttons/arrowButton.svelte";
+	import ArrowButtonLeft from "$lib/components/input/buttons/arrowButtonLeft.svelte";
 
 </script>
-<h1>Minemaker UI Library</h1>
 
-<ArrowButton>Hello World</ArrowButton>
+<main class="p-12 space-y-8">
+	<h1>Minemaker UI Library</h1>
+	
+	<div>
+		<p>ArrowButtonLeft</p>
+		<ArrowButtonLeft>Hello World</ArrowButtonLeft>
+	</div>
+</main>
