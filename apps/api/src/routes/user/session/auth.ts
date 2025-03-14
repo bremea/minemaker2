5eaa2523ba4778c9f8bfb847c6d5e7db54fdc2bc
bremea <bremeagames@gmail.com>
2025-03-13 21:28:24 -0400

    fix auth code returning undefined

--- apps/api/src/routes/user/session/auth.ts
+++ apps/api/src/routes/user/session/auth.ts
@@ -13,7 +13,7 @@ export default (app: ElysiaApp) =>
 					headers: {
 						'Content-Type': 'application/x-www-form-urlencoded'
 					},
-					body: `client_id=${process.env.MICROSOFT_CLIENT_ID}&scope=XboxLive.signin%20XboxLive.offline_access&code=${refreshToken.value}&redirect_uri=${process.env.MICROSOFT_CLIENT_REDIRECT_URI}&grant_type=authorization_code&client_secret=${process.env.MICROSOFT_CLIENT_SECRET}`
+					body: `client_id=${process.env.MICROSOFT_CLIENT_ID}&scope=XboxLive.signin%20XboxLive.offline_access&code=${body.code}&redirect_uri=${process.env.MICROSOFT_CLIENT_REDIRECT_URI}&grant_type=authorization_code&client_secret=${process.env.MICROSOFT_CLIENT_SECRET}`
 				}
 			);
 
