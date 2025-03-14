f602c6dc49812667915d03557b667ec4cb7ae260
bremea <bremeagames@gmail.com>
2025-03-13 22:11:57 -0400

    update caller lib

--- apps/website/src/routes/auth/+page.svelte
+++ apps/website/src/routes/auth/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-		import type { PageProps } from './$types';
+	import type { PageProps } from './$types';
+	import { setContext } from 'svelte';
 	import Navbar from '$lib/components/navigation/navbar.svelte';
 
 	let { data }: PageProps = $props();
+
+	
 </script>
 
 <Navbar />
 
-<a href={data.authLink} class="underline rounded">Login with Microsoft</a>
+<a href={data.authLink} class="rounded underline">Login with Microsoft</a>
