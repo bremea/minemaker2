fb682a6d05b6fff026e2de8ae78af12034ee04a3
bremea <bremeagames@gmail.com>
2025-03-13 21:15:00 -0400

    temp auth page

---
+++ apps/website/src/routes/auth/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Navbar from '$lib/components/navigation/navbar.svelte';
+	import { MICROSOFT_CLIENT_ID, MICROSOFT_CLIENT_REDIRECT_URI } from '$env/static/private';
+</script>
+
+<Navbar />
+
+<a href="https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?client_id=${MICROSOFT_CLIENT_ID}&response_type=code&redirect_uri=${MICROSOFT_CLIENT_REDIRECT_URI}&response_mode=query&scope=XboxLive.signin%20XboxLive.offline_access" class="underline rounded">Login with Microsoft</a>
