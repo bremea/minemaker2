d3fd70e60f59fd281d8f77dbe0ddbf6055e76919
bremea <bremeagames@gmail.com>
2025-07-06 17:20:57 -0400

    add update data to edit page

--- apps/website/src/routes/(app)/studio/projects/[id]/edit/+page.svelte
+++ apps/website/src/routes/(app)/studio/projects/[id]/edit/+page.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
+	import { getApiClient } from '$lib/state.svelte.js';
+	import { updateProject } from '@minemaker/caller';
 	import { Button, ImageUpload, Input, Radio, Tags, Textarea } from '@minemaker/ui';
 
 	let { data } = $props();
 
 	let name = $state(data.project?.name);
 	let description = $state(data.project?.description);
+	let isPublic = $state(data.project?.public);
 	let image: FileList | undefined = $state();
+
+	let loading = $state(false);
 
 	async function onsubmit(event: SubmitEvent) {
 		event.preventDefault();
+		loading = true;
+
+		const newData: { name?: string; description?: string; public?: boolean } = {};
+		if (name !== data.project?.name) newData.name = name;
+		if (description !== data.project?.description) newData.description = description;
+		if (isPublic !== data.project?.public) newData.public = isPublic;
+
+		const updatedData = await updateProject(getApiClient()!, data.id, newData);
+
+		loading = false;
+		data.project = updatedData;
 	}
 </script>
 
@@ -27,7 +43,7 @@
 			Project name
 		</Input>
 
-		<Textarea placeholder="..." class="w-full" maxlength={1000}>Description</Textarea>
+		<Textarea placeholder="..." class="w-full" maxlength={1000} bind:value={description}>Description</Textarea>
 
 		<Button type="submit" class="w-full justify-center text-center">Save Changes</Button>
 	</form>
