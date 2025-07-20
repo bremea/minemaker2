5c7948db1698cd7a4f3009322addafb9e8b97d73
bremea <bremeagames@gmail.com>
2025-07-20 18:43:57 -0400

    update db pool conf to support multi statement queries

--- libraries/db/src/connection.ts
+++ libraries/db/src/connection.ts
@@ -12,5 +12,6 @@ export const pool = mysql.createPool({
 	queueLimit: 0,
 	enableKeepAlive: true,
 	keepAliveInitialDelay: 0,
-	supportBigNumbers: true
+	supportBigNumbers: true,
+	multipleStatements: true
 });
