3d4c8f4d83afe9f457aadc8c90ed01bb3d9d4458
bremea <bremeagames@gmail.com>
2025-07-20 18:39:48 -0400

    update builder api wrapper to include build claims

--- apps/builder/api.go
+++ apps/builder/api.go
@@ -4,19 +4,28 @@ import (
 	"github.com/bremea/minemaker2/libraries/callergo"
 )
 
+func ClaimBuild(a callergo.ApiClient, id string) error {
+	return callergo.UpdateBuild(a, id, callergo.UpdateBuildBodyData{
+		Status:  "In Progress",
+		Success: false,
+	})
+}
+
 func BuildFail(a callergo.ApiClient, id string, log string) error {
 	return callergo.UpdateBuild(a, id, callergo.UpdateBuildBodyData{
-		Status:  "Failed",
-		Success: false,
-		Log:     log,
+		Status:   "Failed",
+		Success:  false,
+		Log:      log,
+		Finished: true,
 	})
 }
 
 func BuildSuccess(a callergo.ApiClient, id string, time int, obj string, log string) error {
 	return callergo.UpdateBuild(a, id, callergo.UpdateBuildBodyData{
-		Status:  "Completed",
-		Success: true,
-		Object:  obj,
-		Log:     log,
+		Status:   "Completed",
+		Success:  true,
+		Object:   obj,
+		Log:      log,
+		Finished: true,
 	})
 }
