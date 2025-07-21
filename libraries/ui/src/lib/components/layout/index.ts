dc572621a94e95fa17287fcfaffb0fa993238028
bremea <bremeagames@gmail.com>
2025-07-21 15:13:04 -0400

    popup element

--- libraries/ui/src/lib/components/layout/index.ts
+++ libraries/ui/src/lib/components/layout/index.ts
@@ -1,2 +1,3 @@
 export { default as BuildInfo } from './buildInfo.svelte';
 export { default as CopyCode } from './copyCode.svelte';
+export { default as Popup } from './popup.svelte';
---
+++ libraries/ui/src/lib/components/layout/popup.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+	import FluentDismiss20Filled from '~icons/fluent/dismiss-20-filled';
+
+	export interface PopupProps extends HTMLAttributes<HTMLDivElement> {
+		class?: string;
+		open?: boolean;
+	}
+
+	function clickOutside(node: HTMLElement) {
+		const handleClick = (event: MouseEvent) => {
+			if (!node.contains(event.target as Node) && open) {
+				open = false;
+			}
+		};
+
+		document.addEventListener('click', handleClick, true);
+
+		return {
+			destroy() {
+				document.removeEventListener('click', handleClick, true);
+			}
+		};
+	}
+
+	let { children, class: className, open = false, ...others }: PopupProps = $props();
+</script>
+
+{#if open}
+	<div
+		class="fixed top-0 left-0 z-50 flex h-screen w-screen items-center justify-center bg-black/50"
+	>
+		<div
+			class={`relative min-w-[400px] rounded-lg border-2 border-gray-600 bg-gray-700 p-8 shadow-lg ${className}`}
+			use:clickOutside
+			{...others}
+		>
+			<button
+				class="group absolute top-2 right-2 flex cursor-pointer items-center justify-center rounded-lg p-2 transition-all hover:bg-gray-500 active:scale-95"
+				onclick={() => open = false}
+			>
+				<FluentDismiss20Filled class="text-gray-400 transition-all group-hover:text-white" />
+			</button>
+			{@render children?.()}
+		</div>
+	</div>
+{/if}
