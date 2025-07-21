22f1604d3acf0eac7b43095e91ea755518a712f8
bremea <bremeagames@gmail.com>
2025-07-21 15:25:13 -0400

    external indicator on link element

--- libraries/ui/src/lib/components/navigation/links/link.svelte
+++ libraries/ui/src/lib/components/navigation/links/link.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
 	import type { HTMLAnchorAttributes } from 'svelte/elements';
+	import FluentOpen20Filled from '~icons/fluent/open-20-filled';
 
 	let { children, class: className, ...others }: HTMLAnchorAttributes = $props();
 </script>
 
-<a class={`text-mm-blue underline hover:text-mm-blue-light transition-all cursor-pointer ${className}`} {...others}>
+<a
+	class={`text-mm-blue hover:text-mm-blue-light inline-flex cursor-pointer items-end space-x-2 underline transition-all ${className}`}
+	{...others}
+>
 	{@render children?.()}
+	{#if others.target == '_blank'}
+		<FluentOpen20Filled/>
+	{/if}
 </a>
